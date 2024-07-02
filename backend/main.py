da12617a762c73f1cee777f31962d443571c82e1
laguzman <laguzman@espol.edu.ec>
2024-07-02 08:52:28 -0500

    ssdsd

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from prisma import Prisma
 from fastapi.middleware.cors import CORSMiddleware
-from controllers.note_controller import router as note_router
-
 app = FastAPI()
 
 app.add_middleware(
@@ -12,5 +12,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(note_router)
+class NoteCreate(BaseModel):
+    body: str
+    archived: bool
+    tag: str
 
+class NoteRead(BaseModel):
+    id: str
+    email: str
+    name: str
+
+
+@app.get("/")
+async def get_note():
+    async with Prisma() as db:
+        data = await db.note.find_many()
+    return data
+
+
+@app.post("/")
+async def create_note(note: NoteCreate):
+    async with Prisma() as db:
+        data = await db.note.create(
+            data=note.model_dump()
+        )
+    return data
+
+
+@app.put("/{id}")
+async def update_note(id: str, note: NoteCreate):
+    async with Prisma() as db:
+        data = await db.note.update(
+            where={"id": id},
+            data=note.model_dump()
+        )
+    return data
+
+
+@app.delete("/{id}")
+async def update_note(id: str,):
+    async with Prisma() as db:
+        data = await db.note.delete(
+            where={"id": id}
+        )
+    return data
